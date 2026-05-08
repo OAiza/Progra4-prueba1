0523c1ad29b6318633faf4c2ecdc7ee67440a808
OAiza <power777pro@gmai.com>
2026-05-07 22:21:55 -0600

    controlador value

---
+++ Progra4-prueba1/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TuProyecto.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CalculadoraController : ControllerBase
+    {
+        // SUMA
+        [HttpGet("sumar")]
+        public IActionResult Sumar(int num1, int num2)
+        {
+            int resultado = num1 + num2;
+
+            return Ok(new
+            {
+                Operacion = "Suma",
+                Resultado = resultado
+            });
+        }
+
+        // RESTA
+        [HttpGet("restar")]
+        public IActionResult Restar(int num1, int num2)
+        {
+            int resultado = num1 - num2;
+
+            return Ok(new
+            {
+                Operacion = "Resta",
+                Resultado = resultado
+            });
+        }
+    }
+}
